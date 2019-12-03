696eb5391f0e2ae967095b1ecc742807f46ddf07
connor worthen <connorw220@protonmail.com>
2019-12-03 03:40:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,12 +23,14 @@ attr_accessor :name, :artist, :genre
     end
   end
 
-  def self.find_by_name
-    Song.all.find{|song| song.name == name}
+  def self.find_by_name(name)
+    @@all.find do |song|
+        song.name == name
+    end
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
 
   def save
