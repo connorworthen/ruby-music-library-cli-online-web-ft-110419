2e66bf36cec8239e587bba295775291328225adf
connor worthen <connorw220@protonmail.com>
2019-12-03 03:45:50 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,22 +1,22 @@
 require 'bundler'
 Bundler.require
 
-module Concerns
-  module Findable
+module Concerns::Findable
 
 
-  def find_by_name
-
+  def find_by_name(name)
+    all.find do |object|
+        object.name == name
+    end
   end
 
 
 
 
-  def find_or_create_by_name
-
+  def find_or_create_by_name(name)
+    find_by_name(name) || self.create(name)
   end
 
-  end
+
 end
-
 require_all 'lib'
