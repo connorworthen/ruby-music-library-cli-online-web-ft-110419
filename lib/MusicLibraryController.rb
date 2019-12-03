642ea4b2dfb8df91b5a6bb66291137ccd812ae00
connor worthen <connorw220@protonmail.com>
2019-12-03 06:56:42 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -97,7 +97,7 @@ class MusicLibraryController
     input = gets.strip.to_i
     #if (input > 0) && (input <= list_of_songs.size)
     #if (input >= 1) && (input <= list_of_songs.size)
-    if (1..Song.all.length).include?(input >= 1) && (input <= list_of_songs.size)
+    if (1..Song.all.length).include?(input > 0) && (input <= list_of_songs.size)
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
