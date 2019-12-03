aee381539801fd02e9382d4ce63e650c139c2df6
connor worthen <connorw220@protonmail.com>
2019-12-03 06:24:31 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -41,16 +41,8 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-    end
-
-    songs_sorted_by_name = Song.all.sort_by do |song|
-      song.name
-    end
-    songs_sorted_by_name.each.with_index(1) do |song,index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    end
+    listed_songs = Song.all.collect.sort_by{|song| song.name}
+    listed_songs.uniq.each_with_index{|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 
   def list_artists
