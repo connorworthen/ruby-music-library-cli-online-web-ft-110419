9292aecfa3d2f5b33cee05cc0ac31ac3ac6d5325
connor worthen <connorw220@protonmail.com>
2019-12-03 02:29:42 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Genre
+
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destory_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    new_genre = Genre.new(name)
+    new_genre.save
+    new_genre
+  end
+end
