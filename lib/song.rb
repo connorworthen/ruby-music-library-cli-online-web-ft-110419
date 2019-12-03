b5ce640f62eef3b46dd45e45bebcc56727704849
connor worthen <connorw220@protonmail.com>
2019-12-03 02:29:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Song
+
+attr_accessor :name, :artist, :genre
+
+@@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destory_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+end
